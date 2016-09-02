86a90645ca4e81c474365c0fb8493183eba43f1f
Tim Seckinger <seckinger.tim@gmail.com>
2016-09-02 20:25:18 +0200

    add NodeManager

---
+++ src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/node/NodeManager.groovy
@@ -0,0 +1,59 @@
+package com.github.jeysal.gradle.plugin.graphviz.node
+
+import com.moowork.gradle.node.NodeExtension
+import org.gradle.api.Project
+
+/**
+ * Encapsulates the node and npm viz setup.
+ * Provides a task that installs the viz binaries.
+ *
+ * @author Tim Seckinger
+ * @since 9/2/16
+ */
+class NodeManager {
+    public static final String NODE_PLUGIN_ID = 'com.moowork.node'
+    public static final String NODE_VERSION = '6.5.0'
+
+    private final Project project
+
+    NodeManager(Project project) {
+        this.project = project
+    }
+
+    /**
+     * Applies and configures the gradle-node-plugin (if not already applied).
+     *
+     * @param project The project to apply the gradle-node-plugin to
+     */
+    void prepareNode() {
+        if (!project.pluginManager.hasPlugin(NODE_PLUGIN_ID)) {
+            project.pluginManager.apply(NODE_PLUGIN_ID)
+            NodeExtension node = project.extensions.node
+
+            node.download = true
+            node.version = NODE_VERSION
+            node.nodeModulesDir = project.gradle.startParameter.projectCacheDir ?:
+                    new File(project.projectDir, '.gradle')
+        }
+    }
+
+    /**
+     * Adds the vizSetup tasks to the specified project.
+     *
+     * @param project The project to add the tasks to
+     * @return The added task
+     */
+    VizSetupTask addVizSetupTask() {
+        project.tasks.create(VizSetupTask.NAME, VizSetupTask)
+    }
+
+    /**
+     * Returns the directory the viz executables are installed into.
+     * Note that this may still change during project configuration.
+     *
+     * @return The installation directory
+     */
+    File getBinDir() {
+        new File((File) project.extensions.node.nodeModulesDir, '.bin')
+    }
+}
