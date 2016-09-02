64fdc576e950d6bef96a436957dc3cd813a91f06
Tim Seckinger <seckinger.tim@gmail.com>
2016-09-02 13:44:55 +0200

    add plugin class and properties

---
+++ src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/GraphvizPlugin.groovy
@@ -0,0 +1,15 @@
+package com.github.jeysal.gradle.plugin.graphviz
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author Tim Seckinger
+ * @since 9/2/16
+ */
+class GraphvizPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
