4f4ed88e9bdc1e7a08f6ae41ae3c37be33e0573b
Tim Seckinger <seckinger.tim@gmail.com>
2016-09-04 13:33:40 +0200

    add GraphvizExtension useVizJs, executablePath

---
+++ src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/GraphvizExtension.groovy
@@ -0,0 +1,10 @@
+package com.github.jeysal.gradle.plugin.graphviz
+
+/**
+ * @author Tim Seckinger
+ * @since 9/2/16
+ */
+class GraphvizExtension {
+    boolean useVizJs = true
+    String executablePath = ''
+}
