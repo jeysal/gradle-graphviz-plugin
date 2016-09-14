0463cf481bd7cb3a4aa5ff2be1c029fd658f4088
Tim Seckinger <seckinger.tim@gmail.com>
2016-09-14 22:40:03 +0200

    add GraphvizSourceFileVisitor

---
+++ src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/exec/GraphvizSourceFileVisitor.groovy
@@ -0,0 +1,41 @@
+package com.github.jeysal.gradle.plugin.graphviz.exec
+
+import com.github.jeysal.gradle.plugin.graphviz.GraphvizTask
+import org.gradle.api.file.FileVisitDetails
+import org.gradle.api.file.FileVisitor
+import org.gradle.api.tasks.TaskExecutionException
+
+/**
+ * @author Tim Seckinger
+ * @since 9/14/16
+ */
+class GraphvizSourceFileVisitor implements FileVisitor {
+    private GraphvizTask graphviz
+
+    GraphvizSourceFileVisitor(GraphvizTask graphviz) {
+        this.graphviz = graphviz
+    }
+
+    @Override
+    void visitDir(FileVisitDetails dirDetails) {
+        // Do nothing
+    }
+
+    @Override
+    void visitFile(FileVisitDetails fileDetails) {
+        def target = fileDetails.relativePath.getFile(graphviz.outputDir)
+        def cmd = [graphviz.executablePath,
+                   '-K', graphviz.layout,
+                   '-T', graphviz.format,
+                   '-o', target.path,
+                   fileDetails.path]
+
+        def proc = cmd.execute()
+        proc.consumeProcessOutputStream(System.out as OutputStream)
+        proc.consumeProcessErrorStream(System.err as OutputStream)
+        if (proc.waitFor()) {
+            fileDetails.stopVisiting()
+            throw new TaskExecutionException(graphviz, new RuntimeException("graphviz command $cmd failed"))
+        }
+    }
+}
