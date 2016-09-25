859b6140001838860d683e98d5d275d772d32d55
Tim Seckinger <seckinger.tim@gmail.com>
2016-09-25 13:28:54 +0200

    add AsciidoctorManager

---
+++ src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/asciidoctor/AsciidoctorManager.groovy
@@ -0,0 +1,26 @@
+package com.github.jeysal.gradle.plugin.graphviz.asciidoctor
+
+import com.github.jeysal.gradle.plugin.graphviz.node.VizSetupTask
+import org.gradle.api.Project
+
+/**
+ * @author Tim Seckinger
+ * @since 9/24/16
+ */
+class AsciidoctorManager {
+    private final Project project
+
+    AsciidoctorManager(final Project project) {
+        this.project = project
+    }
+
+    /**
+     * If present, tells asciidoctor where to find Graphviz.
+     */
+    void registerGraphviz() {
+        def asciidoctor = project.tasks.findByName 'asciidoctor'
+
+        asciidoctor?.dependsOn VizSetupTask.NAME
+        asciidoctor?.attributes graphvizdot: project.tasks.findByName('graphviz').executablePath
+    }
+}
