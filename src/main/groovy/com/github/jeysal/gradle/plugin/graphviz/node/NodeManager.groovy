1c4c44295d96cdb9cf391577b7b5c8b52e6c76ea
Tim Seckinger <seckinger.tim@gmail.com>
2017-01-26 20:12:19 +0100

    node version 7.2.0 -> 7.4.0

--- src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/node/NodeManager.groovy
+++ src/main/groovy/com/github/jeysal/gradle/plugin/graphviz/node/NodeManager.groovy
@@ -12,7 +12,7 @@ import org.gradle.api.Project
  */
 class NodeManager {
     public static final String NODE_PLUGIN_ID = 'com.moowork.node'
-    public static final String NODE_VERSION = '7.2.0'
+    public static final String NODE_VERSION = '7.4.0'
 
     private final Project project
 
